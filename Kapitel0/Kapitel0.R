7a9f93d2f7781c8aa0e481aef8e53aa32cb22dec
asluchych <anatolblr@gmail.com>
2021-12-21 20:41:02 +0100

    Update Kapitel0.R to slide 42

--- Kapitel0/Kapitel0.R
+++ Kapitel0/Kapitel0.R
@@ -33,3 +33,53 @@ factorial(6)
 # Folie 38: (Numerische) Vektoren
 
 c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+seq(from = 1, to = 10, by = 1)
+
+# binärer Operator
+seq(1, 10)
+
+1:10
+
+seq(26, by = 2, length.out = 5)
+
+seq(to = 26, by = 2, length.out = 5)
+
+seq(26, by = -2, length.out = 5)
+
+# Folie 40: Oft genutzte mathematische Funktionen
+
+min(1:10)
+
+cummin(c(15, 20, 10, 30, 5))
+
+max(1:10)
+
+cummax(c(15, 20, 10, 30, 5))
+
+sum(1:10)
+
+cumsum(1:10)
+
+prod(1:10)
+
+cumprod(1:10)
+
+# Folie 41: Runden und Trunkieren
+
+round(1183.63, digits = 1)
+
+round(1183.63, digits = -2)
+
+signif(1183.63, digits = 3)
+
+floor(1183.63)
+
+ceiling(1183.63)
+
+trunc(1183.63)
+
+trunc(-1183.63)
+
+# Folie 42: Wiederholungen in Vektoren
+
